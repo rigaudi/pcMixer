5a73600bcfdec168c12b1b3c8f77df63fabff4a6
Juanimurano <juani.murano@gmail.com>
2016-10-16 21:25:10 -0300

    Add files via upload

---
+++ ModeloFormulario.java
@@ -0,0 +1,81 @@
+//modelo que se utiliza en el formulario
+package ar.edu.grupoesfera.cursospring.modelos;
+
+public class ModeloFormulario {
+	
+	private String pregunta1;
+	private String pregunta2;
+	private String pregunta3;
+	private String pregunta4;
+	private String pregunta5;
+	private String pregunta6;
+	
+	
+	
+	public void setPregunta1(String opcion){
+		
+		this.pregunta1 = opcion;
+		
+	}
+	
+	public String getPregunta1(){
+		
+		return this.pregunta1;
+	}
+	
+	public void setPregunta2(String opcion){
+		
+		this.pregunta2 = opcion;
+		
+	}
+	
+	public String getPregunta2(){
+		
+		return this.pregunta2;
+	}
+	
+public void setPregunta3(String opcion){
+		
+		this.pregunta3 = opcion;
+		
+	}
+	
+	public String getPregunta3(){
+		
+		return this.pregunta3;
+	}
+	
+	public void setPregunta4(String opcion){
+		
+		this.pregunta4 = opcion;
+		
+	}
+	
+	public String getPregunta4(){
+		
+		return this.pregunta4;
+	}
+
+public void setPregunta5(String opcion){
+		
+		this.pregunta5 = opcion;
+		
+	}
+	
+	public String getPregunta5(){
+		
+		return this.pregunta5;
+	}
+	
+	public void setPregunta6(String opcion){
+		
+		this.pregunta6 = opcion;
+		
+	}
+	
+	public String getPregunta6(){
+		
+		return this.pregunta6;
+	}
+	
+}
