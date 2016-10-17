72d7462f13a379f92ddce684eb596d6ca2b3d326
Juanimurano <juani.murano@gmail.com>
2016-10-16 21:26:33 -0300

    Add files via upload

--- proyecto-base-spring/src/main/java/ar/edu/grupoesfera/cursospring/modelos/ModeloFormulario.java
+++ proyecto-base-spring/src/main/java/ar/edu/grupoesfera/cursospring/modelos/ModeloFormulario.java
@@ -3,16 +3,79 @@ package ar.edu.grupoesfera.cursospring.modelos;
 
 public class ModeloFormulario {
 	
-	private char pregunta1;
+	private String pregunta1;
+	private String pregunta2;
+	private String pregunta3;
+	private String pregunta4;
+	private String pregunta5;
+	private String pregunta6;
 	
-	public void setPregunta1(char opcion){
+	
+	
+	public void setPregunta1(String opcion){
 		
 		this.pregunta1 = opcion;
+		
 	}
 	
-	public char getPregunta1(){
+	public String getPregunta1(){
 		
 		return this.pregunta1;
 	}
+	
+	public void setPregunta2(String opcion){
+		
+		this.pregunta2 = opcion;
+		
+	}
+	
+	public String getPregunta2(){
+		
+		return this.pregunta2;
+	}
+	
+public void setPregunta3(String opcion){
+		
+		this.pregunta3 = opcion;
+		
+	}
+	
+	public String getPregunta3(){
+		
+		return this.pregunta3;
+	}
+	
+	public void setPregunta4(String opcion){
+		
+		this.pregunta4 = opcion;
+		
+	}
+	
+	public String getPregunta4(){
+		
+		return this.pregunta4;
+	}
 
+public void setPregunta5(String opcion){
+		
+		this.pregunta5 = opcion;
+		
+	}
+	
+	public String getPregunta5(){
+		
+		return this.pregunta5;
+	}
+	
+	public void setPregunta6(String opcion){
+		
+		this.pregunta6 = opcion;
+		
+	}
+	
+	public String getPregunta6(){
+		
+		return this.pregunta6;
+	}
+	
 }
